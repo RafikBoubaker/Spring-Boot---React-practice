5570769afae4d2c6638391100feae4e685c48a48
RafikBoubaker <boubaker.rafik@gmail.com>
2021-09-07 15:25:25 +0100

    update V0.7

--- PPMToolFullStack(FrontEnd)/src/components/projectBoard/ProjectTasks/ProjectTask.js
+++ PPMToolFullStack(FrontEnd)/src/components/projectBoard/ProjectTasks/ProjectTask.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 
 export default class ProjectTask extends Component {
     render() {
+const { project_task} =this.props;
+
+
         return (
             <div>
-                
+                <div className="card mb-1 bg-light">
+
+                        <div className="card-header text-primary">
+                            ID:{project_task.projectSequence} -- Priority: {project_task.priorityString}
+                        </div>
+                        <div className="card-body bg-light">
+                            <h5 className="card-title">{project_task.summary}</h5>
+                            <p className="card-text text-truncate ">
+                                {project_task.acceptanceCriteria}
+                            </p>
+                            <a  className="btn btn-primary">
+                                View / Update
+                            </a>
+
+                            <button className="btn btn-danger ml-4">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+
             </div>
         )
     }
